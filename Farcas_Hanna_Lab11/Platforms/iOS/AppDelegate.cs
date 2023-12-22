59618379e4789894ab2cbc8fbd1d30bed0321f7f
Hanna Farcas <hanna.farcas@gmail.com>
2023-12-22 22:04:11 +0200

    Add project files.

---
+++ Farcas_Hanna_Lab11/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Farcas_Hanna_Lab11
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
